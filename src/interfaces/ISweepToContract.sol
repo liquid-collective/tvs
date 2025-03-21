ab7479e3f1b0e22a1869b77e8af933fd8a60f43f
Koderholic <meetenebelioluchi@gmail.com>
2025-03-21 18:14:02 +0400

    feat: support sweep to a beneficiary contract

---
+++ src/interfaces/ISweepToContract.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Proprietary
+pragma solidity 0.8.28;
+
+/// @title SweepToContract Interface (v1)
+/// @notice Interface for the TVS beneficiary contract to receive ETH from the TVS.
+/// @dev This interface is used to receive ETH from the TVS contract.
+/// @dev The TVS contract is the withdrawal credential of a set of validators in the system.
+interface ISweepToContract {
+    
+    /// @notice Allows a contract to receive ETH from TVS via the `sweepToContract` function.
+    /// @dev This function MUST be implemented by the TVS beneficiary contract, inorder to use the `sweepToContract` function of the TVS.
+    function receiveETHFromTVS() payable external;
+}
