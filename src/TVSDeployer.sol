19b419fc18b2c7c58be89842ddebe0c65a051583
Mischa Tuffield <mischa@mmt.me.uk>
2026-02-20 11:47:13 +0000

    This 0.8.34

--- src/TVSDeployer.sol
+++ src/TVSDeployer.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Proprietary
-pragma solidity 0.8.29;
+pragma solidity 0.8.34;
 
 import { Clones } from "openzeppelin-contracts/contracts/proxy/Clones.sol";
 import { TVSClone } from "./TVSNonUpgradeable/TVSClone.sol";
